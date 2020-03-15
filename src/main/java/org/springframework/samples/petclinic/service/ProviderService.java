c00354b7b08f5ab61e3be29b2f7ffc5bc1d95fa8
Carlos Cote <carcotmed@alum.us.es>
2020-03-15 16:00:22 +0100

    HU-15: Done

--- src/main/java/org/springframework/samples/petclinic/service/ProviderService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ProviderService.java
@@ -2,11 +2,18 @@ package org.springframework.samples.petclinic.service;
 
 
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.samples.petclinic.model.Product;
 import org.springframework.samples.petclinic.model.Provider;
+import org.springframework.samples.petclinic.repository.ProductRepository;
 import org.springframework.samples.petclinic.repository.ProviderRepository;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +22,9 @@ public class ProviderService {
 
 	@Autowired
 	private ProviderRepository providerRepo;
+	
+	@Autowired
+	private ProductRepository productRepo;
 
 	@Transactional(readOnly = true)
 	public Iterable<Provider> findAll() {
@@ -30,6 +40,19 @@ public class ProviderService {
 	@Transactional(readOnly = true)
 	public Provider findProviderById(int id) throws DataAccessException {
 		return providerRepo.findProviderById(id);
+	}
+
+	@Transactional
+	public void deleteProvider(Provider provider) {
+		providerRepo.delete(provider);
+		
+	}
+
+	public Collection<Product> findAllProductsByProviderId(int providerId) {
+		
+		List <Product> products = new ArrayList <Product> (productRepo.findAllByProviderId (providerId));
+		
+		return products;
 	}
 
 }
