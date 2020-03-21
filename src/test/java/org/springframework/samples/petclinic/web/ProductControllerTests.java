bfcc4044a3ec2d41b068eb6232bb25ab463850de
Moises <moipanloz@alum.us.es>
2020-03-21 17:13:16 +0100

    Test-HU-031: Fixed named, did Order and Product Controller tests

---
+++ src/test/java/org/springframework/samples/petclinic/web/ProductControllerTests.java
@@ -0,0 +1,51 @@
+package org.springframework.samples.petclinic.web;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.samples.petclinic.configuration.SecurityConfiguration;
+import org.springframework.samples.petclinic.service.ProductService;
+import org.springframework.samples.petclinic.service.ProviderService;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers=ProductController.class,
+excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = WebSecurityConfigurer.class),
+excludeAutoConfiguration= SecurityConfiguration.class)
+public class ProductControllerTests {
+
+	@Autowired
+	private ProductController productController;
+
+	@MockBean
+	private ProductService productService;
+	
+	@MockBean
+	private ProviderService providerService;
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	// ========================== Create ===========================
+	
+	@WithMockUser(value = "spring")
+    @Test
+    void testShowProductCreateHtml() throws Exception{
+		mockMvc.perform(get("/products/new")).andExpect(status().isOk())
+		.andExpect(model().attributeExists("providers"))
+		.andExpect(view().name("products/editProduct"));
+	}
+    
+    
+	
+	
+}
