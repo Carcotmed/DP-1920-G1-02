653c678ebfe2e02732be3d26c9a844cf4cb5ec20
Moises <moipanloz@alum.us.es>
2020-05-24 16:14:02 +0200

    Performance Tests - E-007: Done

---
+++ src/test/java/org/springframework/samples/petclinic/performance/simulations/dp2/PT-HU030.scala
@@ -0,0 +1,112 @@
+package dp2
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class PTHU030 extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://www.dp2.com")
+		.inferHtmlResources(BlackList(""".*.png""", """.*.js""", """.*.ico""", """.*.css"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-ES,es;q=0.9")
+		.upgradeInsecureRequestsHeader("1")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.136 Safari/537.36")
+
+	val headers_0 = Map(
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_2 = Map(
+		"Accept" -> "image/webp,image/apng,image/*,*/*;q=0.8",
+		"Proxy-Connection" -> "keep-alive")
+
+	val headers_3 = Map(
+		"Origin" -> "http://www.dp2.com",
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+
+	object Home {
+		val home = exec(http("Home")
+			.get("/")
+			.headers(headers_0))
+		.pause(21)
+	}
+	
+	
+	object LoginOw {
+		val loginOw = exec(
+			http("LoginOw")
+			.get("/login")
+			.headers(headers_0)
+			.check(css("input[name=_csrf]","value").saveAs("stoken"))
+		).pause(13)
+		.exec(
+		http("LoggedOw")
+			.post("/login")
+			.headers(headers_3)
+			.formParam("username", "owner1")
+			.formParam("password", "0wn3r")
+			.formParam("_csrf", "${stoken}")
+		).pause(11)
+	}
+	
+	
+	object LoginVet {
+		val loginVet = exec(
+			http("LoginVet")
+			.get("/login")
+			.headers(headers_0)
+			.check(css("input[name=_csrf]","value").saveAs("stoken"))
+		).pause(13)
+		.exec(
+		http("LoggedVet")
+			.post("/login")
+			.headers(headers_3)
+			.formParam("username", "vet1")
+			.formParam("password", "v3t")
+			.formParam("_csrf", "${stoken}")
+		).pause(11)
+	}
+	
+
+	object ProductsList {
+		val productsList = exec(http("ProductsList")
+			.get("/products")
+			.headers(headers_0))
+		.pause(8)
+	}
+	
+	object DeleteProduct {
+		val deleteProduct = exec(http("DeleteProduct")
+			.get("/products/delete/1")
+			.headers(headers_0))
+		.pause(3)
+	}
+	
+	val deleteProductPositive = scenario("HU030Pos").exec(Home.home,
+									LoginVet.loginVet,
+									ProductsList.productsList,
+									DeleteProduct.deleteProduct)			
+									
+	val deleteProductNegative = scenario("HU030Neg").exec(Home.home,
+									LoginOw.loginOw,
+									ProductsList.productsList,
+									DeleteProduct.deleteProduct)	
+									
+	setUp(
+	deleteProductPositive.inject(rampUsers(200) during (10 seconds)),
+	deleteProductNegative.inject(rampUsers(200) during (10 seconds))
+		).protocols(httpProtocol)
+		 .assertions(
+		 	global.responseTime.max.lt(5000),
+		 	global.responseTime.mean.lt(1000),
+		 	global.successfulRequests.percent.gt(95)
+		 )
+
+}
---
+++ src/test/java/org/springframework/samples/petclinic/performance/simulations/dp2/PT-HU033.scala
@@ -0,0 +1,125 @@
+package dp2
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class PTHU033 extends Simulation {
+
+	val httpProtocol = http
+		.baseUrl("http://www.dp2.com")
+		.inferHtmlResources(BlackList(""".*.png""", """.*.js""", """.*.ico""", """.*.css"""), WhiteList())
+		.acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9")
+		.acceptEncodingHeader("gzip, deflate")
+		.acceptLanguageHeader("es-ES,es;q=0.9")
+		.userAgentHeader("Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.136 Safari/537.36")
+
+	val headers_0 = Map(
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val headers_2 = Map(
+		"Accept" -> "image/webp,image/apng,image/*,*/*;q=0.8",
+		"Proxy-Connection" -> "keep-alive")
+
+	val headers_3 = Map(
+		"Origin" -> "http://www.dp2.com",
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	object Home {
+		val home = exec(http("Home")
+			.get("/")
+			.headers(headers_0))
+		.pause(21)
+	}
+	
+	
+	object Login {
+		val login = exec(
+			http("Login")
+			.get("/login")
+			.headers(headers_0)
+			.check(css("input[name=_csrf]","value").saveAs("stoken"))
+		).pause(13)
+		.exec(
+		http("Logged")
+			.post("/login")
+			.headers(headers_3)
+			.formParam("username", "admin1")
+			.formParam("password", "4dm1n")
+			.formParam("_csrf", "${stoken}")
+		).pause(11)
+	}
+	
+	object ProductsList {
+		val productsList = exec(http("ProductsList")
+			.get("/products")
+			.headers(headers_0))
+		.pause(8)
+	}
+
+	object EditProductPos {
+		val editProductPos = exec(http("EditProductPos")
+			.get("/products/edit/2")
+			.headers(headers_0)
+			.check(css("input[name=_csrf]","value").saveAs("stoken"))
+			).pause(6)
+		.exec(http("ProductUpdatedPos")
+			.post("/products/edit/2")
+			.headers(headers_3)
+			.formParam("name", "Collar")
+			.formParam("price", "26.5")
+			.formParam("quantity", "5")
+			.formParam("allAvailable", "true")
+			.formParam("_allAvailable", "on")
+			.formParam("provider", "1")
+			.formParam("enabled", "true")
+			.formParam("_csrf", "${stoken}"))
+
+	}
+	
+	object EditProductNeg {
+		val editProductNeg = exec(http("EditProductNeg")
+			.get("/products/edit/2")
+			.headers(headers_0)
+			.check(css("input[name=_csrf]","value").saveAs("stoken"))
+			).pause(6)
+		.exec(http("ProductUpdatedNeg")
+			.post("/products/edit/2")
+			.headers(headers_3)
+			.formParam("name", "Collar")
+			.formParam("price", "26.5")
+			.formParam("quantity", "-5")
+			.formParam("allAvailable", "true")
+			.formParam("_allAvailable", "on")
+			.formParam("provider", "1")
+			.formParam("enabled", "true")
+			.formParam("_csrf", "${stoken}"))
+
+	}
+	
+		val updateProductPositive = scenario("HU033Pos").exec(Home.home,
+									Login.login,
+									ProductsList.productsList,
+									EditProductPos.editProductPos)			
+									
+		val updateProductNegative = scenario("HU033Neg").exec(Home.home,
+									Login.login,
+									ProductsList.productsList,
+									EditProductNeg.editProductNeg)		
+									
+	setUp(
+	updateProductPositive.inject(rampUsers(200) during (10 seconds)),
+	updateProductNegative.inject(rampUsers(200) during (10 seconds))
+		).protocols(httpProtocol)
+		 .assertions(
+		 	global.responseTime.max.lt(5000),
+		 	global.responseTime.mean.lt(1000),
+		 	global.successfulRequests.percent.gt(95)
+		 )
+		
+	
+}
